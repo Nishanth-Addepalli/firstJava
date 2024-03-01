744f064a0728df4768a122b950aa68b47e8d69bf
Nishanth-Addepalli <141755468+Nishanth-Addepalli@users.noreply.github.com>
2024-03-01 19:43:28 +0530

    Create diff.c

---
+++ diff.c
@@ -0,0 +1,9 @@
+//C Program To Subtract Two Numbers
+#include<stdio.h>
+int main(){
+  int num1 = 10, num2 = 5, difference;
+  
+  difference = num1 - num2;
+  printf("Difference of %d and %d is: %d", num1, num2, difference);
+  return 0;
+}
